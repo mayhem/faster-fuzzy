01c597a66e530dacad073a13a0e55e18cdd16cf0
Robert Kaye <rob@musicbrainz.org>
2025-03-25 11:17:46 +0100

    For testing

---
+++ vec.cpp
@@ -0,0 +1,31 @@
+#include "tfidf_vectorizer.hpp"
+
+int main()
+{
+    std::vector<std::string> documents = {"This is the first document.", 
+                                          "This document is the second document.", 
+                                          "And this is the third one.", 
+                                          "Is this the first document?"};
+
+    TfIdfVectorizer tfidfvectorizer;
+    arma::mat X = tfidfvectorizer.fit_transform(documents);
+    X.print("TF-IDF Matrix");
+
+    std::map<std::string, double> idfs = tfidfvectorizer.get_idf_();
+    std::map<std::string, size_t> vocab = tfidfvectorizer.get_vocabulary_();
+
+    std::cout << "Training vocabulary:" << std::endl;
+    for (auto const& x : vocab)
+    {
+        std::cout << x.first << " ";
+    }std::cout << std::endl;
+
+
+    std::cout << "IDF values:" << std::endl;
+    for (auto const& x : idfs)
+    {
+        std::cout << x.first << " = " << x.second << std::endl;;
+    }
+
+    return 0;
+}
