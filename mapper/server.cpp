d038c92d00ea1d1e0b8514c7b24cf52a892dcb7a
Robert Kaye <rob@musicbrainz.org>
2025-04-17 17:56:44 +0200

    Interim check in -- starting to write real artist search stuff

--- mapper/server.cpp
+++ mapper/server.cpp
@@ -6,25 +6,41 @@
 #include "mbid_mapping.hpp"
 #include "index_cache.hpp"
 #include "utils.hpp"
+#include "encode.hpp"
 #include "levenshtein.hpp"
+
+// Jeannie & Jimmy Cheatham and The Sweet Baby Blues Band
 
 int main(int argc, char *argv[])
 {
+    EncodeSearchData encode;
 
-#if 0    
-    if (argc != 2) {
-        printf("Usage: builder <index_dir>\n");
+    if (argc != 4) {
+        printf("Usage: builder <index_dir> <artist_name> <recording_name>\n");
         return -1;
     }
     string index_dir(argv[1]);
+    string artist_name(argv[2]);
+    string recording_name(argv[3]);
    
     log("load artist indexes");
     ArtistIndex artist_index(index_dir);
     artist_index.load();
-    log("artist indexes loaded");
     
-    string q("portishead");
-    artist_index.search(q);
+    auto encoded = encode.encode_string(artist_name); 
+    if (encoded.first.size() == 0) {
+        printf("stupid artists are not supported yet.\n")
+        return 0;
+    }
+    string artist_name(encoded[0]);
+    string artist_remainder(encoded[2]);
+    
+    vector<IndexResult> res;
+    res = artist_index->search(artist_name, .5, true);
+    printf("num results: %lu\n", res.size());
+    for(auto & row : res) {
+        printf("%d: %.2f\n", row.id, row.distance); 
+    }
     int artist_credit_id = 65;
     
     IndexCache cache(25);
@@ -56,6 +72,5 @@ int main(int argc, char *argv[])
     for(auto & row : res) {
         printf("%d: %.2f\n", row.id, row.distance); 
     }
-#endif    
     return 0;
 }
